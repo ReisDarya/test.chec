f1ed5152ef75d91f11a94f7d959e689379c6dab8
ReisDarya <dashenka.kazanceva@gmail.com>
2023-08-10 20:43:59 +0700

    gaim

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ console.log(count(object))
 console.log(nameOfCity(object))
 console.log(minmax(object))
 console.log(hottestDay(object))
+console.log(hottestCity(object))
   // END
 }
 function convertToObject(content){
@@ -33,9 +34,21 @@ const hottestDay = (obj) => {
   const hottest = obj.sort((a, b) => b.Max_Temperature - a.Max_Temperature)[0];
   return `HottestDay: ${hottest.Date} ${hottest.City}`
 }
+const hottestCity = (obj) => {
+  const hottest = obj.reduce((acc, item) => {
+    const prop = item.City.replace(' ', '_');
+    acc[prop] = (acc[prop] ?? 0) + Number(item.Max_Temperature);
+    return acc;
+  }, {})
+  const a = Object.values(hottest).sort((a, b) => b - a)[0];
+  return `HottestCity: ${Object.keys(hottest).find(key => hottest[key] === a)}`;
+}
+
 
 //Выведите количество записей с данными в переданном файле. Учтите, что первая строчка в CVS файле является заголовочной, она не содержит данных и не должна учитываться. Вывод утилиты будет таким:
 //Выведите список городов, для которых есть информация о погоде в переданном файле. Города в этом списке отсортированы в алфавитном порядке.
 //Выведите минимальную и максимальную влажность, которая встречается в данных переданного файла.
 //Выведите дату самой жаркой погоды и город, в котором была зафиксирована эта температура
+//Выведите самый жаркий город, основываясь на средней температуре в городе за дни указанные в файле. Средняя температура считается относительно значения `Max Temperature`.
+//С технической точки зрения, вам нужно посчитать среднюю температуру по каждому городу за указанную дату. Затем нужно определить в каком городе эта температура выше.
 
